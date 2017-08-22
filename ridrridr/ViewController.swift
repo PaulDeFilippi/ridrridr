dce6138b27e3c85cde1d864cfdb0180dceec4c04
Paul Defilippi <BlindFaith@pauls-imac.local>
2017-08-21 23:25:24 -0400

    Initial Commit

---
+++ ridrridr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ridrridr
+//
+//  Created by Paul Defilippi on 8/21/17.
+//  Copyright © 2017 Paul DeFilippi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
