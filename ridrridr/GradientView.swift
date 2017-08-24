92a084289a3684262ca0593a150270a869d3f857
Paul Defilippi <BlindFaith@Pauls-iMac.local>
2017-08-23 23:03:42 -0400

    Added a custom gradient to a UIView

---
+++ ridrridr/GradientView.swift
@@ -0,0 +1,28 @@
+//
+//  GradientView.swift
+//  ridrridr
+//
+//  Created by Paul Defilippi on 8/23/17.
+//  Copyright © 2017 Paul DeFilippi. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    let gradient = CAGradientLayer()
+    
+    override func awakeFromNib() {
+        setupGradientView()
+    }
+    
+    func setupGradientView() {
+        gradient.frame = self.bounds
+        gradient.colors = [UIColor.white.cgColor, UIColor.init(white: 1.0, alpha: 0.0).cgColor]
+        gradient.startPoint = CGPoint.zero
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        gradient.locations = [0.8, 1.0]
+        self.layer.addSublayer(gradient)
+    }
+
+}
